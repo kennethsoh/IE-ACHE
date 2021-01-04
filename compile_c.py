dbc22ff4a0d0646f6719e5476c05dbace16dfc65
kennethsoh <sgguy26@gmail.com>
2021-01-04 13:44:27 +0800

    reindent compile_c.py

--- compile_c.py
+++ compile_c.py
@@ -6,60 +6,60 @@ import glob
 
 qn = str(input("Is BOOST Library installed? (yes/no) ")).lower()
 if qn == "no":
-	opsystem  = str(input("Are you on Linux or macOS? (linux/macos) ")).lower()
-	if opsystem == "linux":
-		os.system("apt-get install libboost-all-dev -y")
-		
-		files = glob.glob("*/*.c")
-		if len(files) <= 0:
-			print("No c files found!")
-			sys.exit()
+    opsystem  = str(input("Are you on Linux or macOS? (linux/macos) ")).lower()
+    if opsystem == "linux":
+        os.system("apt-get install libboost-all-dev -y")
 
-		for filename in files:
-			filestriped = filename[:-2]
-			os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
-		
-			print(f"\nCompiling {filename}")
-	
-	os.system("chmod u+x */*")	
-	print(f"\n{len(files)} files compiled. Please check for errors in compliation.")
-	
-	elif opsystem == "macos":
-		os.system("brew install boost")
-		
-		files = glob.glob("*/*.c")
-		if len(files) <= 0:
-			print("No c files found!")
-			sys.exit()
+        files = glob.glob("*/*.c")
+        if len(files) <= 0:
+            print("No c files found!")
+            sys.exit()
 
-		for filename in files:
-			filestriped = filename[:-2]
-			os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
-		
-			print(f"\nCompiling {filename}")
-	
-	os.system("chmod u+x */*")	
-	print(f"\n{len(files)} files compiled. Please check for errors in compliation.")
-	else:
-		print("You did not specify an operating system (linux/macos). BOOST will not be installed.")
-		print("No action taken. Have a nice day :)")
+            for filename in files:
+                filestriped = filename[:-2]
+                os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
+
+                print(f"\nCompiling {filename}")
+
+        os.system("chmod u+x */*")
+        print(f"\n{len(files)} files compiled. Please check for errors in compliation.")
+
+    elif opsystem == "macos":
+        os.system("brew install boost")
+
+        files = glob.glob("*/*.c")
+        if len(files) <= 0:
+            print("No c files found!")
+            sys.exit()
+
+        for filename in files:
+            filestriped = filename[:-2]
+            os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
+
+            print(f"\nCompiling {filename}")
+
+        os.system("chmod u+x */*")
+        print(f"\n{len(files)} files compiled. Please check for errors in compliation.")
+    else:
+        print("You did not specify an operating system (linux/macos). BOOST will not be installed.")
+        print("No action taken. Have a nice day :)")
 
 elif qn == "yes":
-	files = glob.glob("*/*.c")
-	
-	if len(files) <= 0:
-		print("No c files found!")
-		sys.exit()
+    files = glob.glob("*/*.c")
 
-	for filename in files:
-		filestriped = filename[:-2]
-		os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
-		
-		print(f"\nCompiling {filename}")
-	
-	
-	os.system("chmod u+x */*")	
-	print(f"\n{len(files)} files compiled. Please check for errors in compliation.")
+    if len(files) <= 0:
+        print("No c files found!")
+        sys.exit()
+
+    for filename in files:
+        filestriped = filename[:-2]
+        os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
+
+        print(f"\nCompiling {filename}")
+
+
+    os.system("chmod u+x */*")
+    print(f"\n{len(files)} files compiled. Please check for errors in compliation.")
 
 else:
-	print("No action taken. Have a nice day :)")
+    print("No action taken. Have a nice day :)")
