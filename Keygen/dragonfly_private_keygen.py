586fb125587857a2cec40fca76c1aa1f86d85254
JoshuaKhooWX <73164041+JoshuaKhooWX@users.noreply.github.com>
2021-05-16 14:33:15 +0800

    Update dragonfly_private_keygen.py

--- Keygen/dragonfly_private_keygen.py
+++ Keygen/dragonfly_private_keygen.py
@@ -1094,7 +1094,6 @@ def handshake():
             break
         else:
             connection.close()
-            sock.close()
             continue
 
 #######################################################################
